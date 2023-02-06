26bb26c327d3959a05e4627f03344b554640e627
paramedics <unconfigured@null.spigotmc.org>
2023-02-06 10:21:42 +0500

    Added 'base' property to Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/netbook-landing-page/',
+
   plugins: [ ],
 
   resolve: {
